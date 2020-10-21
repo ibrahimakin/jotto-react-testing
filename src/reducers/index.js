9c40103b2547e8068b630d113c4237aba49bfd05
ibrahim AKIN <ibrahim-akin@msn.com>
2020-10-21 23:53:38 +0300

    Set up guessWords tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import success from './successReducers';
+import guessedWords from './guessedWordsReducers';
 
 export default combineReducers({
     success,
+    guessedWords
 });
