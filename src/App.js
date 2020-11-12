16fd829178363b98f0f887dcea05c89c4a7f48bd
ibrahim AKIN <ibrahim-akin@msn.com>
2020-11-12 17:07:01 +0300

    Add language context and test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import hookActions from './actions/hookActions';
+import languageContext from './contexts/languageContext';
+
 import Input from './components/Input';
+import LanguagePicker from './components/LanguagePicker';
 
 function reducer (state, action) {
     switch (action.type) {
         case "setSecretWord":
             return { ...state, secretWord: action.payload };
-
+        case "setLanguage":
+            return { ...state, language: action.payload };
         default:
             throw new Error(`Invalid action type: ${action.type}`);
         //break;
@@ -16,11 +20,15 @@ function reducer (state, action) {
 function App () {
     const [state, dispatch] = React.useReducer(
         reducer,
-        { secretWord: null }
+        { secretWord: null, language: 'en' }
     );
 
     const setSecretWord = (secretWord) => {
         return dispatch({ type: "setSecretWord", payload: secretWord });
+    };
+
+    const setLanguage = (language) => {
+        return dispatch({ type: "setLanguage", payload: language });
     };
 
     React.useEffect(() => {
@@ -43,8 +51,12 @@ function App () {
 
     return (
         <div className="container" data-test="component-app">
-            <Input secretWord={state.secretWord} />
-            <h3>{state.secretWord}</h3>
+            <h1>Jotto</h1>
+            <languageContext.Provider value={state.language}>
+                <LanguagePicker setLanguage={setLanguage} />
+                <Input secretWord={state.secretWord} />
+                <h3>{state.secretWord}</h3>
+            </languageContext.Provider>
         </div>
     );
 }
--- src/Congrats.js
+++ src/Congrats.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import languageContext from './contexts/languageContext';
+import stringsModule from './helpers/strings';
+
 /** 
  * Functional react component for congratulatory message.
  * @function 
@@ -8,11 +12,12 @@ import PropTypes from 'prop-types';
 */
 
 const Congrats = (props) => {
+    const language = React.useContext(languageContext);
     if (props.success) {
         return (
             <div data-test="component-congrats" className="alert alert-success">
                 <span data-test="congrats-message">
-                    Congratulations! You guessed the word!
+                    {stringsModule.getStringByLanguage(language, 'congrats')}
                 </span>
             </div>
         );
@@ -22,7 +27,7 @@ const Congrats = (props) => {
             <div data-test="component-congrats" />
         );
     }
-}
+};
 
 Congrats.propTypes = {
     success: PropTypes.bool.isRequired,
