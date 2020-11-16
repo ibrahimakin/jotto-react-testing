df15f4080efb887fedaa9fd4d0797dea8fe6d804
ibrahim AKIN <ibrahim-akin@msn.com>
2020-11-16 21:50:20 +0300

    Update

--- frontend_reactjs_jotto_app/src/actions/hookActions.js
+++ frontend_reactjs_jotto_app/src/actions/hookActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getSecretWord = async (setSecretWord) => {
-    const response = await axios.get('http://localhost:3030');
+    const response = await axios.get('https://random-word-server.herokuapp.com/');
     setSecretWord(response.data);
 };
 
--- frontend_reactjs_jotto_app/src/actions/index.js
+++ frontend_reactjs_jotto_app/src/actions/index.js
@@ -32,7 +32,7 @@ export const guessWord = (guessedWord) => {
 
 export const getSecretWord = () => {
     return (dispatch) => {
-        return axios.get('http://localhost:3030')
+        return axios.get('https://random-word-server.herokuapp.com/')
             .then(response => {
                 dispatch({
                     type: actionTypes.SET_SECRET_WORD,
